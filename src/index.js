fd3aba5ef7665195cb276ea40989226193b3476f
xseth415 <51835407+xseth415@users.noreply.github.com>
2019-06-21 23:18:36 +0200

    Get-Request
    Get Request along with getJSONString()

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-const express = require("express");
-const app = express();
+const http = require("http");
+const httpStatus = require("http-status-codes");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app = http.createServer();
+
+const port = 3000;
+
+const getJSONString = obj => {
+  return JSON.stringify(obj, null, 2);
+};
+
+app.on("request", (req, res) => {
+  res.writeHead(httpStatus.OK, { "Content-Type": "text/html" });
+
+  let responseMessage = "<h1>This will show on the screen.</h1>";
+
+  console.log(`Method: ${getJSONString(req.method)}`);
+  console.log(`URL: ${getJSONString(req.url)}`);
+  console.log(`Header: ${getJSONString(req.headers)}`);
+
+  res.end(responseMessage);
 });
 
-app.listen(3000, () => {
-  console.log("Express web app on localhost:3000");
-});
+app.listen(port);
+
+console.log(`The server has started and is listening on port number: ${port}`);
